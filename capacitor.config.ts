6a2b9de3d22760ebbbde8e7d864f32d7d14fe1b4
prashant <prashantgangwar1001@gmail.com>
2022-02-13 00:03:55 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'inventory_management_service',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
