37313263bb6be376e34b2225bd60e6ff9257ab79
prashant <prashantgangwar1001@gmail.com>
2022-02-20 03:57:48 +0530

    dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
+import React from 'react';
+import { IonApp, 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonFooter, 
+  IonMenu,
+  IonList,
+  IonListHeader,
+  IonMenuToggle,
   IonIcon,
   IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
+  IonPage ,
+  IonButtons,
+  IonButton,
   IonTabButton,
-  IonTabs,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+  IonItem, 
+  setupIonicReact} from '@ionic/react';
+
+import { home,logOut } from 'ionicons/icons'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -38,38 +43,48 @@ setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+    <IonMenu content-id="main-content">
+    <IonHeader>
+      <IonToolbar color="primary">
+        <IonTitle>Menu</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+
+    <IonContent>
+      <IonList>
+        <IonListHeader>
+          Navigate
+        </IonListHeader>
+        <IonMenuToggle auto-hide="false">
+          <IonItem button>
+            <IonIcon slot="start" icon={home}></IonIcon>
+            <IonLabel>
+              Home
+            </IonLabel>
+          </IonItem>
+        </IonMenuToggle>
+      </IonList>
+    </IonContent>
+  </IonMenu>
+
+  <IonPage className="ion-page" id="main-content">
+    <IonHeader>
+      <IonToolbar>
+        <IonButtons slot="start">
+          <IonMenuToggle>
+            <IonTabButton>
+              <IonIcon name="apps-outline"></IonIcon>
+            </IonTabButton>
+          </IonMenuToggle>
+        </IonButtons>
+        <IonTitle>Header1</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className="ion-padding">
+      <h1>Main Content</h1>
+      <p>Click the icon in the top left to toggle the menu.</p>
+    </IonContent>
+  </IonPage>
   </IonApp>
 );
 
